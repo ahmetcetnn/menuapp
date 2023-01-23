fdd14cded3ba3e93b3a4791204a3de49a54d3289
ahmetcetnn <ahmetlaw@outlook.com>
2023-01-24 00:49:23 +0300

    navbar hazırladım

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from 'react'
-import "./App.css"
+import React from 'react';
 
-function App() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
+import { Navbar } from './components';
+import './App.css';
 
-export default App
+const App = () => (
+  <div>
+    <Navbar />
+    <Header />
+    <AboutUs />
+    <SpecialMenu />
+    <Chef />
+    <Intro />
+    <Laurels />
+    <Gallery />
+    <FindUs />
+    <Footer />
+  </div>
+);
+
+export default App;
